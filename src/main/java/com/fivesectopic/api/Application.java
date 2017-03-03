8a626b0e443f89c016748983c987a241ee156bf9
nruiz64 <ruiz.nicolas54@gmail.com>
2017-03-03 12:36:40 +0100

    init api

---
+++ src/main/java/com/fivesectopic/api/Application.java
@@ -0,0 +1,18 @@
+package com.fivesectopic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by Nicolas on 03/03/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
